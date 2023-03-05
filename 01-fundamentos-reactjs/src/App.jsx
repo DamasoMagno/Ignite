4eace489254609abf66fdd5e868e506874e045dc
DamasoMagno <limamdamaso@gmail.com>
2023-03-05 14:43:27 -0300

    initial commit

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className="wrapper">
+        <aside></aside>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
---
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import igniteLogo from "../assets/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+  );
+}
